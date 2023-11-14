3405d8abe88af72ac2874991f79bc4b4a51d3140
CoLoDot <constancelouise.gauriau@gmail.com>
2023-11-14 15:37:21 +0100

    :sparkles: Requesting URL Parameters

--- hello.go
+++ hello.go
@@ -3,12 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, world! You've requested:  %s\n", r.URL.Path)
+	router := mux.NewRouter()
+
+	router.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		variables := mux.Vars(r)
+		title := variables["title"]
+		page := variables["page"]
+
+		fmt.Fprintf(w, "You have requested the book: %s on page %s\n", title, page)
+
 	})
 
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":80", router)
 }
